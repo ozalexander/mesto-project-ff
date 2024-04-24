7f66aa0d952594b1cf6ae28bba946f0e51fad1c8
ozalexander <156056519+ozalexander@users.noreply.github.com>
2024-04-25 00:07:02 +0300

    Final

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 const cardsTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
+const deleteThisCard = (evt) => evt.target.closest('.card').remove();
 
-initialCards.forEach((card) => {
+function creatCard(item, { deleteCard }) {
   const cardsElement = cardsTemplate.cloneNode(true);
+  const cardTitle = cardsElement.querySelector('.card__title');
+  const cardImage = cardsElement.querySelector('.card__image');
   const deleteButton = cardsElement.querySelector('.card__delete-button');
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
+  deleteButton.addEventListener('click', deleteCard);
+  return cardsElement;
+}
 
-  cardsElement.querySelector('.card__title').textContent = card.name;
-  cardsElement.querySelector('.card__image').src = card.link;
-  cardsElement.querySelector('.card__image').alt = 'Фотография';
-
-  deleteButton.addEventListener('click', evt => evt.target.parentElement.style.display='none');
-
-  placesList.append(cardsElement);
-})
+initialCards.forEach(card => placesList.append(creatCard(card, { deleteCard : deleteThisCard })));
